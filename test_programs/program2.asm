95eec2b3aa400cb61d300210774c3e63a0871a71
Riki Borders <58313130+RikiBorders@users.noreply.github.com>
2021-07-21 00:32:44 -0500

    Added test programs
    These programs are useful for testing the functionality of the assembler. Very basic test
    programs were omitted from this upload, and each program in the 'test_programs' folder
    contain various nuances that make them unique.

---
+++ test_programs/program2.asm
@@ -0,0 +1,7 @@
+. Weight: 1
+. Needs SIC/XE, based
+start	8000
+BASE	32768
+count:	resw	1
+first:	lda	count			@require based
+end	first
---
+++ test_programs/program4.asm
@@ -0,0 +1,7 @@
+. this is a comment
+    .this is another comment
+first:  start   0
+        add     5
+        ldx     #1
+        +ldt   #4096
+        ldb    @first
